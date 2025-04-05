05b8ac5756878e539d7bac319b99463921b30492
dev-omaFrank <166918947+dev-omaFrank@users.noreply.github.com>
2025-04-06 01:40:25 +0200

    Update page.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,23 +6,23 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
- 
+
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
+  const latestInvoices = await fetchLatestInvoices(); // Fetch the latest invoices
   const {
     numberOfInvoices,
     numberOfCustomers,
     totalPaidInvoices,
     totalPendingInvoices,
   } = await fetchCardData();
- 
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-         <Suspense fallback={<CardsSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
@@ -30,8 +30,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
+        {/* Pass the latestInvoices prop to LatestInvoices component */}
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
+          <LatestInvoices latestInvoices={latestInvoices} />
         </Suspense>
       </div>
     </main>
