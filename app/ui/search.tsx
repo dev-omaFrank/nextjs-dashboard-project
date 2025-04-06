c342ad13bef523580f32b2328e7c0b4a6ceadcbf
dev-omaFrank <166918947+dev-omaFrank@users.noreply.github.com>
2025-04-06 03:05:59 +0200

    Update search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,6 +10,7 @@ export default function Search() {
  
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
